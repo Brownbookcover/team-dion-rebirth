66d88badf2a2d2fcf95a97b28cc6d4b1cb692e46
Dion Tryban <dion@tryban.dev>
2025-10-18 16:31:25 -0400

    feat: make helmet camera sensitivity lower

--- Prefabs/Player/player.gd
+++ Prefabs/Player/player.gd
@@ -12,8 +12,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
-			pivot.rotate_y(-event.relative.x * 0.01)
-			camera.rotate_x(-event.relative.y * 0.01)
+			pivot.rotate_y(-event.relative.x * 0.01 / stats.camera_impedance)
+			camera.rotate_x(-event.relative.y * 0.01 / stats.camera_impedance)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
 
 
