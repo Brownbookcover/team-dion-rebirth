e7fed60252187b778d58e9a8ddc6f004332b0da6
Dion Tryban <dion@tryban.dev>
2025-10-18 12:28:24 -0400

    feat: start on better player controller

--- Prefabs/Player/player.gd
+++ Prefabs/Player/player.gd
@@ -1,11 +1,9 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
 @onready var pivot := $Pivot
 @onready var camera := $Pivot/Camera3D
+
+@export var stats: PlayerStats
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -20,23 +18,22 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		velocity.y = stats.jump_velocity
+	
 	var input_dir := Input.get_vector("player_left", "player_right", "player_forward", "player_back")
 	var direction = (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
+	var target_move_vel = direction * stats.max_move_vel
+	
+	var current_move_vel = Vector3(velocity.x, 0, velocity.z)
+	
+	var new_move_vel = current_move_vel.move_toward(target_move_vel, stats.move_acc * delta)
+	
+	velocity.x = new_move_vel.x
+	velocity.z = new_move_vel.z
+	
 	move_and_slide()
