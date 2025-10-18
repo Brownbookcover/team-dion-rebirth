97d74fa01363f51cd7226e6a318f1f3549a1f2bd
alectrem <tremblayalec14@gmail.com>
2025-10-18 12:49:32 -0600

    adds ocean shaders, ocean manager

---
+++ Scenes/ocean_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@export var depth_range = 20
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	%OceanEnv.depth = clamp(%Player.position.y, -depth_range, depth_range) / depth_range
+	pass
