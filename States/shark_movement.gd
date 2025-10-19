bb902ffad8e2d4a5fadf1446848e3af9319e66f0
08 <mike.ridgeway123@gmail.com>
2025-10-18 20:47:33 -0400

    Deleted state machine stuff and added basic movement

--- States/shark_movement.gd
+++ States/shark_movement.gd
@@ -2,14 +2,14 @@ extends State
 class_name StateChase
 
 @export var shark: CharacterBody3D
-@export var move_speed := 5
+@export var shark_move_speed := 5
 @export var agent_shark: NavigationAgent3D
 var player: CharacterBody3D
 
 func Enter():
 	player = get_tree().get_first_node_in_group("player")
 	
-func Physics_Update(delta: float):
+func _physics_process(delta: float) -> void:
 	agent_shark.target_position = player.global_position
 	var direction = agent_shark.get_next_path_position() - shark.global_position
 	shark.velocity = direction.normalized() * move_speed
--- shark.gd
+++ shark.gd
@@ -1,5 +1,20 @@
 extends CharacterBody3D
 
+@export var shark_move_speed := 5
+@export var agent_shark: NavigationAgent3D
+@export var jumpscare_image: Sprite2D
+var player: CharacterBody3D
 
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	
 func _physics_process(delta: float) -> void:
-	pass
+	agent_shark.target_position = player.global_position
+	var direction = agent_shark.get_next_path_position() - position
+	velocity = direction.normalized() * shark_move_speed
+	move_and_slide()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		jumpscare_image.visible = true
