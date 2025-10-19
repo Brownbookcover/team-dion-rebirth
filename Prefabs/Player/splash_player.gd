3dff9fe548fe9ca9e503766c0ca26c4df82abbef
alectrem <tremblayalec14@gmail.com>
2025-10-19 12:37:30 -0600

    adds more sound

---
+++ Prefabs/Player/splash_player.gd
@@ -0,0 +1,14 @@
+extends AudioStreamPlayer3D
+
+
+var triggered = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if get_parent_node_3d().position.y < 0 and triggered == false:
+		triggered = true
+		play()
