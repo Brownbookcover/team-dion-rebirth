7599a7a6577aaba8e239ac89dd93e7191c6dbcfd
alectrem <tremblayalec14@gmail.com>
2025-10-19 13:11:08 -0600

    Update breath_manager.gd

--- Prefabs/Player/breath_manager.gd
+++ Prefabs/Player/breath_manager.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(%Gauge.start_oxygen, %Gauge.level_float, %breathsound1.stream_paused, %breathsound2.stream_paused, %breathsound3.stream_paused)
+	# print(%Gauge.start_oxygen, %Gauge.level_float, %breathsound1.stream_paused, %breathsound2.stream_paused, %breathsound3.stream_paused)
 	if %Gauge.start_oxygen == true:
 		if %Gauge.level_float > 0.6:
 			%breathsound1.stream_paused = false
